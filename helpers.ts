1518058b6a6fd4b9ee0e6bd69b2c5e71479fed8a
marekpieczarek28-pixel <marekpieczarek28@gmail.com>
2026-03-06 20:31:53 +0100

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:31:24.224Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T19:31:41.082Z
+Commit: Resolve timezone issues in date handling
