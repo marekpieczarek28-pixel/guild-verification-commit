b7313c65f803eef9fcac341dc18c6d5db74bbecc
marekpieczarek28-pixel <marekpieczarek28@gmail.com>
2026-03-06 20:31:28 +0100

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:31:14.581Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T19:31:15.905Z
+Commit: Patch API endpoint error handling
