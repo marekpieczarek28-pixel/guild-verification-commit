839bb52eb8130e3593a6d3a8726c633f6f22dc21
marekpieczarek28-pixel <marekpieczarek28@gmail.com>
2026-03-06 20:33:24 +0100

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:32:45.819Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T19:33:12.189Z
+Commit: Add export functionality for reports
