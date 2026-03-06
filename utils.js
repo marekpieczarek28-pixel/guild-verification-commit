593200c8befdeb560cd0e8c26ecc804f4c352cd5
marekpieczarek28-pixel <marekpieczarek28@gmail.com>
2026-03-06 20:32:24 +0100

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:32:07.887Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T19:32:11.745Z
+Commit: Fix broken links in documentation
