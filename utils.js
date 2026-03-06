271c98346bcbb15ffee9b424ac5833ea9d2052cd
marekpieczarek28-pixel <marekpieczarek28@gmail.com>
2026-03-06 20:33:06 +0100

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:32:41.362Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T19:32:54.203Z
+Commit: Add multi-language support
