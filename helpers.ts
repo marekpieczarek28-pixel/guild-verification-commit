7ceccfa1b22cf58876f0c4ff8e917bd4adf78e95
marekpieczarek28-pixel <marekpieczarek28@gmail.com>
2026-03-06 20:32:58 +0100

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:32:42.759Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-06T19:32:45.819Z
+Commit: Fix CSS styling bug on mobile devices
