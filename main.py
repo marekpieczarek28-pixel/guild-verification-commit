2396b9407999dd70b9e891627cfdefe655f63abf
marekpieczarek28-pixel <marekpieczarek28@gmail.com>
2026-03-06 20:31:32 +0100

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:31:11.816Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T19:31:19.920Z
+Commit: Resolve timezone issues in date handling
